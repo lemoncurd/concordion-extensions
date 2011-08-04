47bed6824971c2c5a8157dc73a0a949080fa2405
Ben Buckley <ben@codemonkey.co.uk>
2011-08-04 23:32:43 +0100

    Added documentation for running the specLinkver via ant

---
+++ src/test/java/spec/uk/co/codemonkey/concordion/specLinker/RunningTest.java
@@ -0,0 +1,9 @@
+package spec.uk.co.codemonkey.concordion.specLinker;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class RunningTest {
+
+}
