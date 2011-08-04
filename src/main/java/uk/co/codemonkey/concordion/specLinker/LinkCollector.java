850f72f183360024ac01e0b185c0f9aaa64447b0
Ben Buckley <ben@codemonkey.co.uk>
2011-08-04 23:28:58 +0100

    Added jvm classpath to SpecLinker task to fix build issues on Debian command line. Removed some unnecessary code. Pass test directories to SpecLinker using command line arguments.

--- src/main/java/uk/co/codemonkey/concordion/specLinker/LinkCollector.java
+++ src/main/java/uk/co/codemonkey/concordion/specLinker/LinkCollector.java
@@ -2,27 +2,17 @@ package uk.co.codemonkey.concordion.specLinker;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.List;
 
 import uk.co.codemonkey.concordion.specLinker.DirectoryWalker.FileListener;
 
 public class LinkCollector implements FileListener {
 
-	private Deque<FileLinks> links = new LinkedList<FileLinks>();
+	private FileTreeNode root;  
+	private FileTreeNode curr;
 	
-	FileTreeNode root;  
-	FileTreeNode curr;
-	
-	public LinkCollector(String rootDirectory, String rootIndex) {
-		//links.push(new FileLinks(new File(rootDirectory, rootIndex)));
-		root = curr = new FileTreeNode(new File(rootDirectory, rootIndex));
-	}
-	
-	Collection<FileLinks> getLinks() {
-		return links;
+	public LinkCollector(File root) {
+		this.root = curr = new FileTreeNode(root);
 	}
 	
 	FileTreeNode getFiles() {
@@ -34,9 +24,6 @@ public class LinkCollector implements FileListener {
 		if(!file.equals(curr.file)) {
 		  curr = curr.add(file);
 		}
-		
-//		fileFound(file);
-//		links.push(new FileLinks(file));
 	}
 
 	@Override
@@ -44,29 +31,13 @@ public class LinkCollector implements FileListener {
 		if(!file.equals(curr.file)) {
 			curr.add(file);
 		}
-		
-//		FileLinks peek = links.peek();
-//		if(links.size() != 1 || !peek.index.equals(file)) {
-//			peek.links.add(file);
-//		}
 	}
 
 	@Override
 	public void leftScopeOfIndex() {
 		curr =  curr.getParent();
-//		FileLinks fileLinks = links.pop();
-//		links.addLast(fileLinks);
 	}
 	
-	static class FileLinks {
-		final File index;
-		final List<File> links = new LinkedList<File>();
-		
-		public FileLinks(File index) {
-			this.index = index;
-		}
-	}
-
 	static class FileTreeNode {
 		private final List<FileTreeNode> children = new ArrayList<FileTreeNode>();
 		private final File file;
--- src/main/java/uk/co/codemonkey/concordion/specLinker/SpecLinker.java
+++ src/main/java/uk/co/codemonkey/concordion/specLinker/SpecLinker.java
@@ -8,46 +8,52 @@ import java.nio.channels.FileChannel;
 
 public class SpecLinker {
 
+	private static final String DEFAULT_INDEX_HTML = "/defaultIndex.html";
+	private static final String ROOT_INDEX = "index.html";
+
 	public void link(String resultDirectory, String specDirectory) throws Exception{
-		String rootIndex = "index.html";
-		
-		createRootIndexIfNecessary(specDirectory, rootIndex);
+		File root = createRootIndexIfNecessary(specDirectory, ROOT_INDEX);
 		
 		DirectoryWalker walker = new DirectoryWalker();	
-		LinkWriter writer = new LinkWriter(new JunitResults(resultDirectory, specDirectory));
-	
-		LinkCollector collector = new LinkCollector(specDirectory, rootIndex);
+		LinkCollector collector = new LinkCollector(root);
 		walker.processDirectory(specDirectory, collector);
-//		for(FileLinks links : collector.getLinks()) {
-//			writer.addLinks(links.index, links.links);
-//		}
+
+		JunitResults junitResults = new JunitResults(resultDirectory, specDirectory);
+		LinkWriter writer = new LinkWriter(junitResults);
 		writer.addLinks(collector.getFiles());
 	}
 
-	private void createRootIndexIfNecessary(String resultDirectory,
-			String rootIndex) throws Exception {
-		
+	private File createRootIndexIfNecessary(String resultDirectory, String rootIndex) throws Exception {
 		File file = new File(resultDirectory,rootIndex);
 		if(!file.exists()) {
-			FileChannel source= null;
-			FileChannel destination= null;
-			try {
-				URI indexURI = this.getClass().getResource("/defaultIndex.html").toURI();
-				source = new FileInputStream(new File(indexURI)).getChannel();
-			    destination = new FileOutputStream(file).getChannel();
-                destination.transferFrom(source, 0, source.size());
-			}
-			finally {
-				if(source != null) source.close();
-				if(destination != null) destination.close();
-			}
+			copyDefaultInto(file);
+		}
+		return file;
+	}
+
+	private void copyDefaultInto(File file) throws Exception {
+		FileChannel source= null;
+		FileChannel destination= null;
+		try {
+			URI indexURI = this.getClass().getResource(DEFAULT_INDEX_HTML).toURI();
+			source = new FileInputStream(new File(indexURI)).getChannel();
+		    destination = new FileOutputStream(file).getChannel();
+		    destination.transferFrom(source, 0, source.size());
+		}
+		finally {
+			if(source != null) source.close();
+			if(destination != null) destination.close();
 		}
 	}
 	
 	public static void main(String[] args) throws Exception {
 		SpecLinker specLinker = new SpecLinker();
-		specLinker.link("target/test-output",  "target/test-output/concordion");
+		
+		if(args.length < 2) {
+			System.err.println(String.format("Usage: java %s <junit result directory> <concordion result directory>", SpecLinker.class.getCanonicalName()));			
+		}
+		
+		specLinker.link(args[0], args[1]);
 	}
-
 	
 }
